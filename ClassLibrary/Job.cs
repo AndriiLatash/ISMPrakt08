3efec4995a1c8179c810b031e2a2780e680dfbee
Andrii <latasandrej@gmail.com>
2022-02-15 16:05:06 +0200

    Made some changes in class Job

--- ClassLibrary/Job.cs
+++ ClassLibrary/Job.cs
@@ -6,5 +6,30 @@ namespace ClassLibrary
 {
     class Job
     {
+        public string CompanyName { set; get; }
+        public Address Address;
+        public int Salary { set; get; }
+        public string WorkingHours { set; get; }
+
+        public Job(string companyName, Address address, int salary, string workingHours)
+        {
+            CompanyName = companyName;
+            Address = address;
+            Salary = salary;
+            WorkingHours = workingHours;
+
+        }
+        public Job(Job job)
+        {
+
+            CompanyName = job.CompanyName;
+            Address = job.Address;
+            Salary = job.Salary;
+            WorkingHours = job.WorkingHours;
+        }
+        public string GetJob()
+        {
+            return $"Company name : {CompanyName}\n Address : {Address.GetAddress()}\n Salary {Salary} UAH\n Working hours : {WorkingHours}";
+        }
     }
 }
